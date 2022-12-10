f23ac605061919bd9a32cd37b342ae2d69fb2771
Максим Рудой <doodoo690@gmail.com>
2022-12-10 20:09:17 +0500

    finish index.html style for all device

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,7 @@
 const headerBtn = document.querySelector('.header__btn'),
       menu = document.querySelector('.menu')
+      bodyMenu = document.querySelector('.page__body')
+
 
       headerBtn.addEventListener('click', () => {
         const exp = "true" === headerBtn.getAttribute('aria-expanded')
@@ -7,7 +9,17 @@ const headerBtn = document.querySelector('.header__btn'),
 
         menu.classList.toggle('menu--opened')
         headerBtn.classList.toggle('header__btn--active')
+        bodyMenu.classList.toggle('page__body--menu')
+
 
         exp ? headerBtn.setAttribute('aria-label', 'Открыть меню')
             : headerBtn.setAttribute('aria-label', 'Закрыть меню')
-      })
+      });
+
+      const menuItem = document.querySelector('.menu__link'),
+            curentListItem = document.querySelector('.logo')
+
+            menuItem.addEventListener('click', () => {
+                curentListItem.classList.toggle('menu__link--current')
+            })
+
